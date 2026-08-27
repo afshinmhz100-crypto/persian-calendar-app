14fefe8bd76563f2a7d60e47bd13a80a58f58335
afshinmhz100-crypto <afshinmhz100@gmail.com>
2026-08-27 13:05:24 +0330

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -50,10 +50,10 @@ android {
     }
   }
 
-  compileOptions {
-    sourceCompatibility = JavaVersion.VERSION_11
-    targetCompatibility = JavaVersion.VERSION_11
-  }
+ compileOptions {
+    sourceCompatibility = JavaVersion.VERSION_17
+    targetCompatibility = JavaVersion.VERSION_17
+}
 
   buildFeatures {
     compose = true
